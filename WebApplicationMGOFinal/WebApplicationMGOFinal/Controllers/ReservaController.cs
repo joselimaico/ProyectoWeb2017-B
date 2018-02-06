4d23d9448fb413c380adf3f6959db6728018eb05
joselimaico <joselimaicotele@gmail.com>
2018-02-06 11:13:01 -0500

    Commit 2
    Cambios en la vista Index y creacion de Operación Edit

--- WebApplicationMGOFinal/WebApplicationMGOFinal/Controllers/ReservaController.cs
+++ WebApplicationMGOFinal/WebApplicationMGOFinal/Controllers/ReservaController.cs
@@ -38,8 +38,52 @@ namespace WebApplicationMGOFinal.Controllers
             return RedirectToAction("Index");
 
         }
+		//Editar
+
+		public ActionResult Edit(int idReserva)
+		{
+			using (ReservasModelContainer db = new ReservasModelContainer())
+			{
+				var std = (from q in db.ReservaSet
+						   where q.IdReserva == idReserva
+						   select q).FirstOrDefault();
+				return View(std);
+			}
+
+		}
+
+		[HttpPost]
+		public ActionResult Edit(Reserva reservastd)
+		{
+			if (ModelState.IsValid)
+			{
+				using (ReservasModelContainer db = new ReservasModelContainer())
+				{
+
+					var u = (from q in db.ReservaSet
+							 where q.IdReserva == reservastd.IdReserva
+							 select q).FirstOrDefault();
+					if (u != null)
+					{
+						u.NombreCliente = reservastd.NombreCliente;
+						u.ApellidoCliente = reservastd.ApellidoCliente;
+						u.CorreoCliente = reservastd.CorreoCliente;
+						u.EstadoReserva = reservastd.EstadoReserva;
+						u.FechaInicio = reservastd.FechaInicio;
+						u.FechaFin = reservastd.FechaFin;
+						u.InstitucionCliente = reservastd.InstitucionCliente;
+						u.TelefonoCliente = reservastd.TelefonoCliente;
+					}
+					db.SaveChanges();
 
 
+				}
+				return RedirectToAction("Index");
+			}
+			return View(reservastd);
 
-    }
+		}
+
+
+	}
 }
